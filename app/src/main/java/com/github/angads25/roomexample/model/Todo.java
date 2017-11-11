5fe0f3e7389b1fcde0074600e3502201b407252e
Angad Singh <angads25@gmail.com>
2017-11-12 01:39:17 +0530

    feat: Added getter and setter methods, chore: Upgraded gradle dependencies

--- app/src/main/java/com/github/angads25/roomexample/model/Todo.java
+++ app/src/main/java/com/github/angads25/roomexample/model/Todo.java
@@ -24,4 +24,36 @@ public class Todo {
 
     @ColumnInfo(name = "time")
     public long timeStamp;
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public String getTodoTitle() {
+        return todoTitle;
+    }
+
+    public void setTodoTitle(String todoTitle) {
+        this.todoTitle = todoTitle;
+    }
+
+    public String getTodoText() {
+        return todoText;
+    }
+
+    public void setTodoText(String todoText) {
+        this.todoText = todoText;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
